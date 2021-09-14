44c7bb4eb8d22056b80ab6e78ac6fb8d4de40298
Ashok <er.ashok23@gmail.com>
2021-09-14 13:32:44 +0530

    syntex error fixed

--- app/Console/Commands/CoolCompanyRegFreelancer.php
+++ app/Console/Commands/CoolCompanyRegFreelancer.php
@@ -84,20 +84,16 @@ class CoolCompanyRegFreelancer extends Command
                   ]
               ];
             $createdFreelancerInfo = $this->createFreelancer($access_token, $data);
-            if(!empty($createdFreelancerInfo))
-            {
+            if(!empty($createdFreelancerInfo)) {
                 //Update Record
                 $resDecode = json_decode($createdFreelancerInfo, true);
                 $studentInfo = StudentDetail::select('id','cool_company_id')->where('user_id', $student->id)->first();
                 $studentInfo->cool_company_id = $resDecode['id'];
                 $studentInfo->save();
                 Log::channel('customlog')->info($student->id.' :Student successfully registered. cool company id: '.$studentInfo->cool_company_id);
-                }
-            else
-            {
+            } else {
                 //Lock Error
                 Log::channel('customlog')->info($student->id.' :Student registration failed. Please Check');
-                }
             }
         }
     }
