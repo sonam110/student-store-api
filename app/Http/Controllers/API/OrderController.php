62e82064b792de507f6aeba116227e26e337faea
Ashok <er.ashok23@gmail.com>
2021-09-17 22:45:07 +0530

    order mail id decrypt

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -325,7 +325,7 @@ class OrderController extends Controller
 		                    	'body' => $body
 		                    ];
 		                    
-		                    Mail::to(Auth::user()->email)->send(new OrderMail($details));
+		                    Mail::to(AES256::decrypt(Auth::user()->email), env('ENCRYPTION_KEY'))->send(new OrderMail($details));
 
 	                    //Mail End
 	                }
