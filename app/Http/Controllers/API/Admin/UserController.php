4b79be9663ddb54106efbb0ed2f7fa20b7f603a8
Ashok <er.ashok23@gmail.com>
2021-10-12 12:21:42 +0530

    screen name changed on admin user status changed

--- app/Http/Controllers/API/Admin/UserController.php
+++ app/Http/Controllers/API/Admin/UserController.php
@@ -311,7 +311,7 @@ class UserController extends Controller
 	            $body =  'User '.$user->title.' status has been successfully updated.';
 
 	            $type = 'User Action';
-	            pushNotification($title,$body,$user,$type,true,'creator','user',$user->id,'my-listing');
+	            pushNotification($title,$body,$user,$type,true,'creator','user',$user->id,'restart');
 	        }
 
 	        DB::commit();
