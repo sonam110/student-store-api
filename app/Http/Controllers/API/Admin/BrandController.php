eef4fdbe37cfaf9e50f9b8dbdf081299721a89a6
Ashok <er.ashok23@gmail.com>
2021-11-09 12:35:41 +0530

    syntex error resolved

--- app/Http/Controllers/API/Admin/BrandController.php
+++ app/Http/Controllers/API/Admin/BrandController.php
@@ -23,11 +23,11 @@ class BrandController extends Controller
             {
                 $brands = $brands->where('name', 'LIKE', '%'.$request->title.'%');
             }
-            if($request->status==0)
+            if($request->status=='verified')
             {
                 $brands = $brands->where('status', 0);
             }
-            elseif($request->status==1)
+            elseif($request->status=='unverified')
             {
                 $brands = $brands->where('status', 1);
             }
