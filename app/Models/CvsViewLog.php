ce4d0497587a2dd3080964882c3138de3a5284ba
Ashok <er.ashok23@gmail.com>
2021-12-14 19:09:28 +0530

    cvs fillable added

--- app/Models/CvsViewLog.php
+++ app/Models/CvsViewLog.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class CvsViewLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id','user_cv_detail_id','user_package_subscription_id','valid_till'];
 }
