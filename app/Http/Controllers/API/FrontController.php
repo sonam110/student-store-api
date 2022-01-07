a87d28a9939a0a5df8eaf3d0b06bf618b75ef570
Ashok <er.ashok23@gmail.com>
2022-01-07 12:58:59 +0530

    revert version chaking

--- app/Http/Controllers/API/FrontController.php
+++ app/Http/Controllers/API/FrontController.php
@@ -347,8 +347,8 @@ class FrontController extends Controller
 	public function getLanguageListForDDL(Request $request)
 	{
 		//removed this after implement the logic
-		//$languages = LangForDDL::orderBy('name', 'ASC')->get();
-		//return response()->json(prepareResult(false, $languages, getLangByLabelGroups('messages','message_user_type_list')), config('http_response.success'));
+		$languages = LangForDDL::orderBy('name', 'ASC')->get();
+		return response()->json(prepareResult(false, $languages, getLangByLabelGroups('messages','message_user_type_list')), config('http_response.success'));
 
 		$allowedVersion = AppSetting::select('allowed_app_version')->first();
 		$allAllowedVersion = json_decode($allowedVersion->allowed_app_version, true);
