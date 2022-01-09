9d97bd87023bf27177d05c8423def14688a7f09f
Ashok <er.ashok23@gmail.com>
2022-01-09 18:38:00 +0530

    deduction policy implemented

--- app/Http/Controllers/API/Contests/ContestApplicationController.php
+++ app/Http/Controllers/API/Contests/ContestApplicationController.php
@@ -187,15 +187,15 @@ class ContestApplicationController extends Controller
                             if($orderedItem)
                             {
                                 $refundOrderItemId = $orderedItem->id;
-                                $refundOrderItemPrice = ($orderedItem->price_after_apply_reward_points) * ($value->deduct_percentage_value)/100;
+                                $refundOrderItemPrice = ($orderedItem->price_after_apply_reward_points) * (100 - $value->deduct_percentage_value)/100;
 
                                 //Update commission and reward
-                                $orderedItem->returned_rewards = ceil($orderedItem->used_item_reward_points * ($value->deduct_percentage_value)/100);
+                                $orderedItem->returned_rewards = ceil($orderedItem->used_item_reward_points * (100 - $value->deduct_percentage_value)/100);
 
-                                $orderedItem->amount_transferred_to_vendor = ceil($orderedItem->amount_transferred_to_vendor * ($value->deduct_percentage_value)/100);
-                                $orderedItem->student_store_commission = ceil($orderedItem->student_store_commission * ($value->deduct_percentage_value)/100);
-                                $orderedItem->cool_company_commission = ceil($orderedItem->cool_company_commission * ($value->deduct_percentage_value)/100);
-                                $orderedItem->vat_amount = ceil($orderedItem->vat_amount * ($value->deduct_percentage_value)/100);
+                                $orderedItem->amount_transferred_to_vendor = ceil($orderedItem->amount_transferred_to_vendor * (100 - $value->deduct_percentage_value)/100);
+                                $orderedItem->student_store_commission = ceil($orderedItem->student_store_commission * (100 - $value->deduct_percentage_value)/100);
+                                $orderedItem->cool_company_commission = ceil($orderedItem->cool_company_commission * (100 - $value->deduct_percentage_value)/100);
+                                $orderedItem->vat_amount = ceil($orderedItem->vat_amount * (100 - $value->deduct_percentage_value)/100);
                                 $orderedItem->save();
 
                                 $refundOrderItemQuantity = $orderedItem->quantity;
