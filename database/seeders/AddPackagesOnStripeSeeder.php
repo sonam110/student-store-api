6e4fdc25587bb4466a10830f9f1353081253dd47
Ashok <er.ashok23@gmail.com>
2022-01-10 20:00:13 +0530

    fresh added

--- database/seeders/AddPackagesOnStripeSeeder.php
+++ database/seeders/AddPackagesOnStripeSeeder.php
@@ -39,6 +39,13 @@ class AddPackagesOnStripeSeeder extends Seeder
                     } else {
                         $amount = $package->price;
                     }
+                    return [
+                        'amount'          => $amount * 100,
+                        'currency'        => $paymentInfo->stripe_currency,
+                        'interval'        => 'day',
+                        'interval_count'  => $package->duration,
+                        'product'         => $createProduct->id,
+                    ];
                     $plan = $stripe->plans->create([
                         'amount'          => $amount * 100,
                         'currency'        => $paymentInfo->stripe_currency,
