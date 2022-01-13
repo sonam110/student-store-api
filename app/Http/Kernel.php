2b2bb883afc495f6854dd5d9286524b5e688f514
Ashok <er.ashok23@gmail.com>
2022-01-13 15:03:30 +0530

    throttle limit changed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            'throttle:100000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
