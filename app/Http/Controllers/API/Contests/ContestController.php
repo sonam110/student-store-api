abcf730923e0c394a827676066bf2301f8526f5e
Ashok <er.ashok23@gmail.com>
2022-01-13 18:30:43 +0530

    filter correct

--- app/Http/Controllers/API/Contests/ContestController.php
+++ app/Http/Controllers/API/Contests/ContestController.php
@@ -837,7 +837,7 @@ class ContestController extends Controller
                 }
                 if(!empty($request->end_date))
                 {
-                    $contests->whereDate('application_end_date', '<=', date('Y-m-d', strtotime($request->end_date)));
+                    $contests->whereDate('application_start_date', '<=', date('Y-m-d', strtotime($request->end_date)));
                 }
 
                 if(!empty($request->free_subscription))
